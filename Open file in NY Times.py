6ac0044e15bdb76a1362e31ac6b863ba0df0ad6a
joeyn256 <126018117+joeyn256@users.noreply.github.com>
2023-08-07 21:01:22 -0400

    Update Open file in NY Times.py

--- Open file in NY Times.py
+++ Open file in NY Times.py
@@ -3,19 +3,22 @@ from bs4 import BeautifulSoup
 url = 'https://www.nytimes.com/?auth=login-google1tap&login=google1tap'
 r = requests.get(url)
 r_html = r.text
+#using requests we get the html code and make it into soup with html parser
 
 soup = BeautifulSoup(r_html, 'html.parser')
 
 
-#p = open("div_results", "w")
-#p.write(str(soup.find_all('div')))
-#class="css-xdandi"><h3'
+#upon inspection of HTML code, title with links are embedded under this:
+#class="css-xdandi"'
 
+#writes a file with the HTML code of NY times version run formatted in a easier way to read with the prettify function
 t= open("NY_Times_HTML Code.txt", "w")
 t.write(soup.prettify())
 
+#this is to find the titles which goes in and looks for the div class defined by the name mentioned in an earlier comment
 article_div_elements = soup.find_all("div", class_="css-xdandi")
 
+#create a dictionary which has relatively fast run-time to store the elements and randomize the process
 Dict1 = {}
 
 for element in article_div_elements:
@@ -26,6 +29,7 @@ for element in article_div_elements:
 
     Dict1[article_title] = article_link
 
+#function chooses random element from dictionary
 random_article_title = random.choice(list(Dict1.keys()))
 
 #opens the webpage
